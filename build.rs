da65e22ee85edd8b135ea5428462ed05e20a745a
Amos Wenger <amoswenger@gmail.com>
2019-04-25 22:58:45 +0200

    Only use libx264 name on windows-msvc

--- build.rs
+++ build.rs
@@ -10,7 +10,16 @@ fn main() {
         lib_path.to_str().unwrap()
     );
 
-    let static_libs = vec!["avutil", "avformat", "avcodec", "swscale", "swresample", "libx264"];
+    let target = std::env::var("TARGET").unwrap();
+    let x264_lib = if target.contains("windows-msvc") {
+        // x264's build scripts produce "libx264.lib" for
+        // some reason. oh well.
+        "libx264"
+    } else {
+        "x264"
+    };
+
+    let static_libs = vec!["avutil", "avformat", "avcodec", "swscale", "swresample", x264_lib];
     for lib in static_libs {
         println!("cargo:rustc-link-lib=static={}", lib);
     }
